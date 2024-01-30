7383498b0546c8f8cd16c9d0986885cd6f4727b9
suhjuho <sspaka0422@gmail.com>
2024-01-30 11:18:09 +0900

    test

--- src/loaders/routers.js
+++ src/loaders/routers.js
@@ -2,8 +2,6 @@ const indexRouter = require("../routes/index");
 
 async function routerLoader(app) {
   app.use("/", indexRouter);
-  console.log("hi");
-  const a = 1;
 }
 
 module.exports = routerLoader;
