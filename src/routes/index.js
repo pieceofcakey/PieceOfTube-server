718d0085e3fb7c2f881472fbf02ad47538446fbe
suhjuho <sspaka0422@gmail.com>
2024-01-30 11:29:59 +0900

    feat

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require("express");
+
 const router = express.Router();
 
-router.get('/', function (req, res, next) {
+router.get("/", function (req, res, next) {
   res.status(200).send({ success: true });
 });
 
