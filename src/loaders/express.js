70111a83bdbc8029701447c0368319e8f436b506
suhjuho <sspaka0422@gmail.com>
2024-01-30 11:17:43 +0900

    test

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -3,9 +3,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 async function expressLoader(app) {
-  app.use(
-    cors(),
-  );
+  app.use(cors());
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
